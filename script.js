3581273e569951f44aa9e817ea924b0990b4e01d
afsha.creator <afshi5874@gmail.com>
2026-01-15 05:36:04 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript function for search button
+function searchBlood() {
+    alert("Searching blood donors...");
+    console.log("Search button clicked");
+}
+
+// JavaScript function for donor registration form
+function registerDonor() {
+    alert("Donor registered successfully!");
+    console.log("Form submitted");
+}
